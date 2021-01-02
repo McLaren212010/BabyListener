f09acbcfc1910e095be209daa9eafd56d1bb4144
mclaren212010 <rmiguelferreira@ipvc.pt>
2021-01-02 17:49:05 +0000

    Register and Login XML's done.

---
+++ app/src/main/java/TP6/babylistener/Register.kt
@@ -0,0 +1,11 @@
+package TP6.babylistener
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
